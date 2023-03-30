d2f5775f5c04b1f74aa306e2d9d9caad6536747b
Anobots <89107294+syahrizalemano@users.noreply.github.com>
2023-03-30 12:44:31 -0700

    Update filters.py

--- helpers/filters.py
+++ helpers/filters.py
@@ -1,10 +1,15 @@
+from pyrogram import Client
+
 from typing import Union, List
 
 from pyrogram import filters
 
 from config import COMMAND_PREFIXES
 
-other_filters = filters.group & ~ filters.edited & ~ filters.via_bot & ~ filters.forwarded
+from pyrogram.handlers import MessageHandler
+
+EditedMessageHandler
+other_filters = filters.group & ~ filters.via_bot & ~ filters.forwarded
 other_filters2 = filters.private & ~ filters.edited & ~ filters.via_bot & ~ filters.forwarded
 
 
